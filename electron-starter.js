37e4f6e2324b15919ca8e1d62ec2e8aea44fa9bc
duboisR <dubois.rom@gmail.com>
2017-06-15 04:52:38 -0400

    Use parsec logo as AppImage launcher icon

--- electron-starter.js
+++ electron-starter.js
@@ -16,7 +16,7 @@ const pjson = require('./package.json')
 let mainWindow
 const createWindow = () => {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600, minWidth: 760, minHeight: 600 })
+  mainWindow = new BrowserWindow({ width: 800, height: 600, minWidth: 760, minHeight: 600, icon: path.join(__dirname, '/public/favicon.png') })
 
   // Create the application menu
   const menu = Menu.buildFromTemplate([
